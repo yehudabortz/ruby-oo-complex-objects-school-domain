d7032fbfcc3a8abffb17168405fe57fbde65f45f
Yehuda Bortz <yehudabortz@icloud.com>
2020-11-04 16:23:56 -0600

    commit tree d5b1d5680d01038f62f32f4a091fce055c301206

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
-# code here!
+# create School class
+
+# initialize School with empty hash called roster
+
+# add_student to an to a key value pair. Key = Grade and value is students in that grade 
+
+# use if else to create new key value pairs if the grade doesnt exist
+
+require 'pry'
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+      @name = name
+      @roster = {}
+    end
+  
+
+    def add_student(name, grade)
+        if roster[grade]
+            roster[grade] << name
+        else
+            roster[grade] = []
+            roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort 
+        sorted_roster = {}
+        roster.each do |grade, students|
+            sorted_roster[grade] = students.sort
+    
+        end
+        sorted_roster
+    end
+
+end
